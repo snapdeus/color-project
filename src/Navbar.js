1a8a7c7172dc944b9669f261a5cc28a6acc11c02
snapdeus <snapdeus@gmail.com>
2023-01-30 16:58:11 -0500

    downgraded react dependencies to follow course, added select & material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
 import Slider, { Range } from 'rc-slider';
-
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 class Navbar extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { format: 'hex' }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(e) {
+        this.setState({ format: e.target.value });
+        this.props.handleChange(e.target.value)
+    }
     render() {
-        const { level, changeLevel } = this.props;
+        const { level, changeLevel, handleChange } = this.props;
+        const { format } = this.state
         return (
             <header className="Navbar">
                 <div className="logo">
@@ -39,8 +50,13 @@ class Navbar extends Component {
 
                     </div>
                 </div>
-
-
+                <div className="select-container">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - RGB(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - RGBA(255,255,255,1.0)</MenuItem>
+                    </Select>
+                </div>
             </header>
         )
     }
