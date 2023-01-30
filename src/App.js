12b1d438ad477091c631a22f2d88e0f609498307
snapdeus <snapdeus@gmail.com>
2023-01-30 12:01:05 -0500

    add color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
-
+import { generatePalette } from './colorHelpers';
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]))
     return (
       <div className='Palette'>
         {/* navbar */}
