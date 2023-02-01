1e6cea9b3ca6877c5efce2351110f5f49cbd3118
snapdeus <snapdeus@gmail.com>
2023-02-01 14:12:09 -0500

    add single color palette and link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 
 
 
@@ -23,7 +24,7 @@ class App extends Component {
 
         <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
         <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
-        <Route exact path="/palette/:paletteId/:colorId" render={() => <h1>SINGLE COLOR PAGE</h1>} />
+        <Route exact path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
       </Switch>
 
       // <div className="Palette-colors">
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -18,7 +18,7 @@ class ColorBox extends Component {
     }
 
     render() {
-        const { name, background } = this.props
+        const { name, background, paletteId, id } = this.props
         const { copied } = this.state
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -37,7 +37,7 @@ class ColorBox extends Component {
                         </button>
                     </div>
                     {/* stop propagation is to keep the transition from including the copy effect */}
-                    <Link to="/" onClick={e => e.stopPropagation}>
+                    <Link to={`/palette/${ paletteId }/${ id }`} onClick={e => e.stopPropagation}>
                         <span className="see-more">MORE</span>
                     </Link>
 
