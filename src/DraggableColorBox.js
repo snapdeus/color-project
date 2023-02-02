86d9b4deef242fee5875d19e346ff2351721fc77
snapdeus <snapdeus@gmail.com>
2023-02-02 16:25:31 -0500

    added form validations

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -18,7 +18,7 @@ const styles = {
 
 
 function DraggableColorBox(props) {
-    return <div className={props.classes.root} style={{ backgroundColor: props.color }}>{props.color} </div>
+    return <div className={props.classes.root} style={{ backgroundColor: props.color }}>{props.name} </div>
 }
 
 
