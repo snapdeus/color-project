1b8e034e450ad7b3a01dde3616da38fdca73b20a
snapdeus <snapdeus@gmail.com>
2023-02-02 12:29:04 -0500

    finish refactor of colorbox styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -58,6 +58,67 @@ const styles = {
         textTransform: "uppercase",
         border: "none",
         opacity: "0"
+    },
+    boxContent: {
+        position: "absolute",
+        padding: "10px",
+        width: "100",
+        left: "0px",
+        bottom: "0px",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+    },
+    copyOverlay: {
+        zIndex: "0",
+        opacity: "0",
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.6s ease-in-out",
+        transform: "scale(0.1)",
+    },
+    showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute",
+    },
+    copyMessage: {
+        position: "fixed",
+        left: "0",
+        right: "0",
+        top: "0",
+        bottom: "0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        opacity: "0",
+        color: "white",
+        "& h1": {
+            fontWeight: "400",
+            textShadow: "1px 2px black",
+            background: "rgba(255,255,255,.2)",
+            width: "100%",
+            textAlign: "center",
+            marginBottom: "0",
+            padding: "1rem",
+            textTransform: "uppercase",
+        },
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100"
+        }
+    },
+    showMessage: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.2s",
     }
 
 }
@@ -82,8 +143,8 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className={classes.ColorBox}>
-                    <div className={`copy-overlay ${ copied && "show" }`} style={{ background }} />
-                    <div className={`copy-msg ${ copied && "show" }`}>
+                    <div className={`${ classes.copyOverlay } ${ copied && classes.showOverlay }`} style={{ background }} />
+                    <div className={`${ classes.copyMessage } ${ copied && classes.showMessage }`}>
                         <h1 className={classes.copyText}>
                             copied!
                         </h1>
@@ -91,8 +152,8 @@ class ColorBox extends Component {
                             {this.props.background}
                         </p>
                     </div>
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div>
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName}>
                                 {name}
                             </span>
